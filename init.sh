b2e6e3e820d94a56b3203ea6ef45e222ac0e0508
Sam Schmalzried <sam.schmalzried@gmail.com>
2019-03-15 16:50:27 -0400

    Script checks for correct distribution
    Currently the file checks if you are running CentOS (any version), and if you are not then it quits.  I should probably fix it so that it checks for CentOS 7 to ensure the correct libraries/dependencies are available, but I'm going to leave it as-is for now.  The check also fails if you are running RHEL despite that also being compatible, so that's also on the TODO.

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-yum install -y git
+
+if [[ $(head -n 1 /etc/os-release) != *"CentOS Linux"* ]]
+then
+	echo "Please run this script on CentOS Linux"
+	exit
+fi
+
+yum install -y git centos-release-scl
+yum install -y rh-python36 rh-python36-python-pip
