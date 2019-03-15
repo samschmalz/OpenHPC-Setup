4293ae356c10fdc2b3e6a632433d4e63d049941a
Sam Schmalzried <sam.schmalzried@gmail.com>
2019-03-15 18:03:01 -0400

    Enabled Python3.6, added sudo to commands
    After Python3.6 is installed, it needs to be enabled in order to be usable.  Otherwise any commands using Python 3 won't be recognized, which would completely prevent the Python-based webpage from running properly.  Additionally, sudo was added to the commands that use it.  Previously the script didn't contain any sudo lines, but that required the user to run it as root or with sudo before the script name.  That meant that when the git repo was cloned, its folder was owned by root.  Adding sudo should prevent commands that don't need root from being run under elevated permissions.

--- init.sh
+++ init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#check if running CentOS
 if [[ $(head -n 1 /etc/os-release) != *"CentOS Linux"* ]]
 then
 	echo "Please run this script on CentOS Linux"
 	exit
 fi
 
-yum install -y git centos-release-scl
-yum install -y rh-python36 rh-python36-python-pip
+#installing necessary packages
+sudo yum install -y git centos-release-scl
+sudo yum install -y rh-python36 rh-python36-python-pip
+
+scl enable rh-python36 bash
+
+git clone https://github.com/samschmalz/OpenHPC-Setup.git
