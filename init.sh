58fd582f8cf754887360a0bb90f876814579983b
Sam Schmalzried <sam.schmalzried@gmail.com>
2019-03-19 17:29:10 -0400

    Added PHP7.3 to the script
    After some consideration I decided that PHP would be easier to set up and run than Python, especially with a virtualenv.  With PHP I simply have to install the modules I need from yum, then I can run `php -S` and get a basic webserver.  Python with Flask will be heavier and will likely be more functionality than I actually need.

--- init.sh
+++ init.sh
@@ -7,17 +7,26 @@ then
 	exit
 fi
 
+#setting up php 7 repo and necessary utils
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum -y install epel-release yum-utils
+
+sudo yum-config-manager --disable remi-php54
+sudo yum-config-manager --enable remi-php73
+
+sudo yum -y install php php-cli
+
 #installing necessary packages
-sudo yum install -y git centos-release-scl
-sudo yum install -y rh-python36 rh-python36-python-pip
+#sudo yum install -y git centos-release-scl
+#sudo yum install -y rh-python36 rh-python36-python-pip
 
 #enable python 3.6
-scl enable rh-python36 bash
-virtualenv -p python36 OpenHPC-web
-cd OpenHPC-web
-source bin/activate
-pip install --upgrade pip
-pip install flask
+#scl enable rh-python36 bash
+#virtualenv -p python36 OpenHPC-web
+#cd OpenHPC-web
+#source bin/activate
+#pip install --upgrade pip
+#pip install flask
 
 git clone https://github.com/samschmalz/OpenHPC-Setup.git
 
