90fa8257fba0a7edca1ab374a725b8b2b2bb3e2a
Sam Schmalzried <sam.schmalzried@gmail.com>
2019-05-10 22:00:41 -0500

    Added new password change page, updated setup page
    After discussion with Dan, I decided that a password change page for the sake of security would be best.  index.php now asks users to change their password, then redirects them to setup.php where they will complete the OpenHPC setup process.

--- index.php
+++ index.php
@@ -1,63 +1,41 @@
 <?php
 	session_start();
-	if (!isset($_SESSION["arch"]))
-	{
-		$_SESSION['arch'] = `uname -p`;
-	}
 
-	if (isset($_POST)) {
-		error_log(json_encode($_POST));
+	if(isset($_POST)) {
+		$pw1 = $_POST["password1"];
+		$pw2 = $_POST["password2"];
+		if (is_null($pw1) || is_null($pw2)) {
+			$message = "Please fill both password fields.";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		}
+		if ($pw1 != $pw2) {
+			$message = "Passwords do not match, please try again.";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		}
+		else {
+			$message = "Thank you!";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+			header("Location: setup.php");
+		}
 	}
 ?>
 
 <html>
-<head>
-	<title>OpenHPC Setup</title>
-</head>
-<body>
-	<form method="post" action="finish.php">
-		<label for="Architecture">CPU Architecture</label>
-		<input type="text" name="Architecture" value=<?php echo $_SESSION['arch']; ?> required><br>
-		<label for="Master-name">Master Node Name</label>
-		<input type="text" name="Master-name" value=<?php echo exec("hostname"); ?> required><br>	
-		<label for="Master-ip">Master node IP</label>
-		<input type="text" name="Master-ip" value=<?php echo exec("hostname -i"); ?> required><br>
-		<br>
-		<label for="compute-count"># of Compute Nodes</label>
-		<input type="number" name="compute-count" id="compute-count" min="1" max="4" required><br>
-		<p>
-		IP Addresses:
-		<div id="ip-block" style="padding-left: 50px">
-			<label for="compute-ip-1">Compute Node 1</label>
-			<input type="text" name="compute-ip-1"><br>
-			<label for="compute-ip-2">Compute Node 2</label>
-			<input type="text" name="compute-ip-2"><br>
-			<label for="compute-ip-3">Compute Node 3</label>
-			<input type="text" name="compute-ip-3"><br>
-			<label for="compute-ip-4">Compute Node 4</label>
-			<input type="text" name="compute-ip-4"><br>
-		</div>
-		</p>
-		<p>
-		MAC Addresses:
-		<div id="mac-block" style="padding-left: 50px">
-			<label for="compute-mac-1">Compute Node 1</label>
-			<input type="text" name="compute-mac-1"><br>
-			<label for="compute-mac-2">Compute Node 2</label>
-			<input type="text" name="compute-mac-2"><br>
-			<label for="compute-mac-3">Compute Node 3</label>
-			<input type="text" name="compute-mac-3"><br>
-			<label for="compute-mac-4">Compute Node 4</label>
-			<input type="text" name="compute-mac-4"><br>
-		</div>
-		</p>
-		<input type="submit" value="Submit">
-	</form>
-</body>
-</html>
+	<head>
+		<title>OpenHPC Password Change</title>
+		<link rel="stylesheet" type="text/css" href="format.css">
+	</head>
+	<body>
+		For security reasons, you should change the root password on your Raspberry Pi, as the default is well-known and insecure.<br>
+		If you've already changed the password, great!  Just enter the same password here and it will "change" it to the same thing.<br>
+		Otherwise, please enter your new password here.
 
-<!--<script>
-	document.getElementById("compute-count").addEventListener("change", function(){
-		document.getElementById("ip-block").forEach(console.log(this.nodeName));		
-	});
-</script>-->
+		<form method="post" action="#">
+			<label for="password1">Password:</label><br>
+			<input type="password" name="password1" required><br>
+			<label for="password2">Confirm:</label><br>
+			<input type="password" name="password2" required><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
--- setup.php
+++ setup.php
@@ -1,19 +1,89 @@
 <?php
 	session_start();
+	if (!isset($_SESSION["arch"]))
+	{
+		$_SESSION['arch'] = `uname -p`;
+	}
+
+	if (isset($_POST)) {
+		error_log(json_encode($_POST));
+	}
 ?>
 
 <html>
 	<head>
-		<title>OpenHPC Web Setup</title>
+		<title>OpenHPC Setup</title>
+		<link rel="stylesheet" type="text/css" href="format.css">
 	</head>
-	<body>
-		<h1>Welcome to the OpenHPC Web Setup Utility!</h1>
-		<?php
-			$hostname = `hostname`;
-			echo "Hostname: $hostname";
-		?>
-
-		<form method="post">
-		</form>
-	</body>
+<body>
+	<h1>OpenHPC Setup Page</h1>
+	<form method="post" action="finish.php">
+		<label for="Architecture">CPU Architecture</label>
+		<button type="button" onclick="ArchDesc()">?</button>
+		<input type="text" name="Architecture" value=<?php echo $_SESSION['arch']; ?> required><br>
+		<label for="Master-name">Master Node Name</label>
+		<input type="text" name="Master-name" value=<?php echo exec("hostname"); ?> required><br>	
+		<label for="Master-ip">Master node IP</label>
+		<input type="text" name="Master-ip" value=<?php echo exec("hostname -i"); ?> required><br>
+		<label for="compute-count"># of Compute Nodes</label>
+		<input type="number" name="compute-count" id="compute-count" min="1" max="4" required><br>
+		<label for="compute-prefix">Prefix for Compute Nodes</label>
+		<button type="button" onclick="PrefixDesc()">?</button>
+		<input type="text" name="compute-prefix"><br>
+		<p>
+			<h3>Compute Node Names:</h3>
+			<div id="name-block" style="padding-left: 50px">
+				<label for="compute-name-1">Compute Node 1</label>
+				<input type="text" name="compute-name-1"><br>
+				<label for="compute-name-2">Compute Node 2</label>
+				<input type="text" name="compute-name-2"><br>
+				<label for="compute-name-3">Compute Node 3</label>
+				<input type="text" name="compute-name-3"><br>
+				<label for="compute-name-4">Compute Node 4</label>
+				<input type="text" name="compute-name-4"><br>
+			</div>
+		</p>
+		<p>
+			<h3>IP Addresses:</h3>
+			<div id="ip-block" style="padding-left: 50px">
+				<label for="compute-ip-1">Compute Node 1</label>
+				<input type="text" name="compute-ip-1"><br>
+				<label for="compute-ip-2">Compute Node 2</label>
+				<input type="text" name="compute-ip-2"><br>
+				<label for="compute-ip-3">Compute Node 3</label>
+				<input type="text" name="compute-ip-3"><br>
+				<label for="compute-ip-4">Compute Node 4</label>
+				<input type="text" name="compute-ip-4"><br>
+			</div>
+		</p>
+		<p>
+			<h3>MAC Addresses:</h3>
+			<div id="mac-block" style="padding-left: 50px">
+				<label for="compute-mac-1">Compute Node 1</label>
+				<input type="text" name="compute-mac-1"><br>
+				<label for="compute-mac-2">Compute Node 2</label>
+				<input type="text" name="compute-mac-2"><br>
+				<label for="compute-mac-3">Compute Node 3</label>
+				<input type="text" name="compute-mac-3"><br>
+				<label for="compute-mac-4">Compute Node 4</label>
+				<input type="text" name="compute-mac-4"><br>
+			</div>
+		</p>
+		<input type="submit" value="Submit">
+	</form>
+</body>
 </html>
+
+<script>
+	/*document.getElementById("compute-count").addEventListener("change", function(){
+		document.getElementById("ip-block").forEach(console.log(this.nodeName));		
+	});*/
+	function PrefixDesc() {
+		let desc = "This is a piece of text that will be the same across all compute nodes.\nFor example, if you have CNode1, CNode2, and CNode3, this would be 'CNode'";
+		alert(desc);
+	}
+	function ArchDesc() {
+		let desc = "This is the CPU Type of your head node, which should be the same as the\ncompute nodes. If you don't know what this should be, then leave it as its\ndefault value.";
+		alert(desc);
+	}
+</script>
